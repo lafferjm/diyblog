400d3b247bb1b7166e1c6399d2461be808643fdf
Justin Lafferty <lafferjm@gmail.com>
2019-04-29 22:21:40 -0500

    register models with admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog.models import BlogAuthor, BlogPost, BlogComment
 
-# Register your models here.
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'post_date')
+    list_filter = ('author', 'post_date')
+
+
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ('blog', 'author', 'post_date', '__str__')
+    list_filter = ('blog', 'author', 'post_date')
+
+
+admin.site.register(BlogAuthor)
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(BlogComment, BlogCommentAdmin)
