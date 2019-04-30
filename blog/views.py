239c1106f5c16f051d2360a3330c58464547b9a4
Justin Lafferty <lafferjm@gmail.com>
2019-04-30 07:54:15 -0500

    basic index page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from blog.models import BlogAuthor, BlogComment, BlogPost
+
 
 def index(request):
-    return render(request, 'blog/index.html')
+    num_authors = BlogAuthor.objects.all().count()
+    num_comments = BlogComment.objects.all().count()
+    num_posts = BlogPost.objects.all().count()
+
+    context = {
+        'num_authors': num_authors,
+        'num_comments': num_comments,
+        'num_posts': num_posts
+    }
+
+    return render(request, 'blog/index.html', context=context)
